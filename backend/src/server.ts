9dd99eb2a983e89c58c27f7c3281d20216aa021e
deepa <deepanshuyadav1811@gmail.com>
2026-08-06 05:21:24 +0530

    setted up the express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,17 @@
+import app from "./app.js";
+import 'dotenv/config.js';
+
+const main = async () => {
+    //connect to db
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+main().catch((err) => {
+    console.error("Error starting the server:", err);
+});
+
 console.log("Hello from server.ts");
