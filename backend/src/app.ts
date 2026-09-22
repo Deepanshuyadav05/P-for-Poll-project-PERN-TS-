78b0b29e98037116df3730179506a4e275d0bff6
deepa <deepanshuyadav1811@gmail.com>
2026-09-22 09:53:52 +0530

    added cors and minor fixes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,14 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./modules/auth/auth.routes.js";
 import pollRoutes from "./modules/polls/poll.route.js";
 import {errorHandler} from "./middlewares/global-error-middleware.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -15,6 +21,8 @@ app.get("/", (req, res) => {
 
 app.use('/api/auth', authRoutes);
 app.use('/api/polls', pollRoutes)
+
+
 
 app.use(errorHandler);
 
